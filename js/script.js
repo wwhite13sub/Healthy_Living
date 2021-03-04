20e24fd21634e8bc2014df154adc077b98103551
Wanda White <wanda.white32@yahoo.com>
2021-03-04 12:06:13 -0600

    adding catch error function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 
-//function for toggle mobile
-
-
-//function for drop down selection
-
 //Button to retrieve request
 let searchButton = document.querySelector("#search")
 searchButton.addEventListener("click", ()=>{
@@ -15,12 +10,16 @@ async function sendData() {
     let secretKey = config.SECRET_KEY;
     
     let searchOutput = document.getElementById("searchBox").value;
-    let response = await fetch(`https://api.edamam.com/search?app_id=${secretKey}&app_key=${MY_ID}&q=${searchOutput}&from=0&to=15`);
+    let response = await fetch(`https://api.edamam.com/search?app_id=${secretKey}&app_key=${MY_ID}&q=${searchOutput}&from=0&to=25`);
     console.log(response)
     let data = await response.json()
     console.log(data)
-    getApiData(data)   
+    getApiData(data)  
+    .catch(errorMessage) 
 }
+   
+
+
 
 function getCard(cardData) {
     return  `
@@ -28,7 +27,7 @@ function getCard(cardData) {
         <img src="${cardData.recipe.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${cardData.recipe.label}</h5>
-            <p class="card-text">Calories: ${cardData.recipe.calories}</p><p>Health Label: ${cardData.recipe.healthLabels}</p>
+            <p class="card-text">Calories: ${cardData.recipe.calories}</p><p><strong>Health Labels:</strong> ${cardData.recipe.healthLabels}</p>
             <a href="${cardData.recipe.url}" class="btn btn-primary">Visit Website</a>
         </div>
     </div>
@@ -41,17 +40,22 @@ function getApiData(data){
     let markup = data.hits.map(getCard).join('');
     // getCard(data.hits[5]);
     document.querySelector("#content").innerHTML = markup;
-    var hits= "";
-        console.log("count",hits.length);
-        var imgCount = hits.length;
-        document.getElementById("imgCount").innerHTML = imgCount;
+    // var hits= "";
+    //     console.log("count",hits.length);
+    //     var imgCount = hits.length;
+    //     document.getElementById("imgCount").innerHTML = imgCount;
        
-        for (i in hits) {
-            $('.imgCount').append(`<img id='img' src='${hits[i].length}'>`)}
+    //     for (i in hits) {
+    //         $('.imgCount').append(`<img id='img' src='${hits[i].length}'>`)}
 
 }
 
 //Function for error message 
-
-
+function errorMessage() {
+    document.querySelector("#content").innerHTML = `
+      <div class="error">
+        <h1><strong>No recipes found please search again.</strong><h1>
+      </div>
+    `;
+  }
 
