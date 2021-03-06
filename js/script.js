725338b9dfbd521a49a10672ebae8c6b67668ccf
Wanda White <wanda.white32@yahoo.com>
2021-03-05 22:30:10 -0600

    fixing fetch function

--- js/script.js
+++ js/script.js
@@ -5,19 +5,19 @@ searchButton.addEventListener("click", ()=>{
     sendData()
 })
 //function for input value
-async function sendData() {
+function sendData() {
     let MY_ID = config.MY_KEY;
     let secretKey = config.SECRET_KEY;
     
     let searchOutput = document.getElementById("searchBox").value;
-    let response = await fetch(`https://api.edamam.com/search?app_id=${secretKey}&app_key=${MY_ID}&q=${searchOutput}&from=0&to=25`);
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    getApiData(data)  
-    .catch(errorMessage) 
+    
+    fetch(`https://api.edamam.com/search?app_id=${secretKey}&app_key=${MY_ID}&q=${searchOutput}&from=0&to=25`)
+        .then( response => response.json() )
+        .then( getApiData )
+        .catch( errorMessage );
+        
 }
-   
+
 
 
 
@@ -27,28 +27,29 @@ function getCard(cardData) {
         <img src="${cardData.recipe.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${cardData.recipe.label}</h5>
-            <p class="card-text">Calories: ${cardData.recipe.calories}</p><p><strong>Health Labels:</strong> ${cardData.recipe.healthLabels}</p>
+            <p class="card-text">Calories: ${Math.round(cardData.recipe.calories)}</p><p>Number of Ingredients:  ${cardData.recipe.ingredients.length}</p>
             <a href="${cardData.recipe.url}" class="btn btn-primary">Visit Website</a>
         </div>
     </div>
     `;
+   
 }
 
 
 //function to return API ID result in a section
 function getApiData(data){
+    if (data.hits.count === 0) {
+        errorMessage();
+        return;
+    }
     let markup = data.hits.map(getCard).join('');
-    // getCard(data.hits[5]);
-    document.querySelector("#content").innerHTML = markup;
-    // var hits= "";
-    //     console.log("count",hits.length);
-    //     var imgCount = hits.length;
-    //     document.getElementById("imgCount").innerHTML = imgCount;
-       
-    //     for (i in hits) {
-    //         $('.imgCount').append(`<img id='img' src='${hits[i].length}'>`)}
-
+    document.querySelector("#content").innerHTML = markup;   
+    console.log("count",data.hits.length);
+    var imgCount = data.hits.length;
+    document.getElementById("imgCount").innerHTML = imgCount;    
+    console.log(data)
 }
+   
 
 //Function for error message 
 function errorMessage() {
@@ -59,3 +60,4 @@ function errorMessage() {
     `;
   }
 
+  
