53576536e9cdf37374e728a0bbaea96c07ae3f2b
Wanda White <wanda.white32@yahoo.com>
2021-03-06 22:28:07 -0600

    updating math round function

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function sendData() {
     let MY_ID = '423bfc7b9770465e0f3a11b2f470b29e';
     let secretKey = '9b3d82cc';    
     let searchOutput = document.getElementById("searchBox").value;    
-    fetch(`https://api.edamam.com/search?app_id=${secretKey}&app_key=${MY_ID}&q=${searchOutput}&from=0&to=25`)
+    fetch(`https://api.edamam.com/search?app_id=${secretKey}&app_key=${MY_ID}&q=${searchOutput}&imageSize=SMALL&from=0&to=25`)
         .then( response => response.json() )
         .then( getApiData )
         .catch( errorMessage );
@@ -30,12 +30,13 @@ function sendData() {
 
 
 function getCard(cardData) {
+    let totalCalorie = (cardData.recipe.calories)/(cardData.recipe.yield);
     return  `
     <div class="card" style="width: 16rem;">
         <img src="${cardData.recipe.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${cardData.recipe.label}</h5>
-            <p class="card-text">Calories: ${Math.round(cardData.recipe.calories)}</p><p>Ingredients:  ${cardData.recipe.ingredients.length}</p>
+            <p class="card-text">Calories: ${Math.round(totalCalorie)}</p><p>Ingredients:  ${cardData.recipe.ingredients.length}</p>
             <a href="${cardData.recipe.url}" target="_blank" class="btn btn-primary">Visit Website</a>
         </div>
     </div>
